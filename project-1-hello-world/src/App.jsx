b443940e8526aba9666e1e55b3456ed3674e60e5
vinkarn <96374145+vinkarn@users.noreply.github.com>
2025-01-06 02:51:13 +0530

    first commit : this is simple helloworld project
    here i learn how to get helloworld on webpage and also make my first custom tag, and some functionality to it. Importing files and there functions was also concerned in here.

---
+++ project-1-hello-world/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Chai from './chai'
+
+function App() {
+  
+  return (
+    <>
+      
+      <div id="root">
+        Hello world!
+      </div>
+      
+      <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ project-1-hello-world/src/chai.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function (){
+    
+    return(
+        <h1>this is chai element working and my first react thing actually working</h1>
+        
+    )
+}
+
