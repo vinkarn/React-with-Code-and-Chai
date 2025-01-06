c57c765d3adb4d5222ac7968dc937813a6ba1f5c
vinkarn <96374145+vinkarn@users.noreply.github.com>
2025-01-06 23:57:50 +0530

    First commit project 3 tailwind and props
    This enabled me to learn and implement tailwind and props over the react app.
    also got to know a lot about how to use useState, its background-fibre, and how to implement and use tailwind over the react app

---
+++ project-3-using-and-props/src/components/card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Card({topic="Science of Chemistry"}){
+  // console.log("props: ", topic)//this was for testing purpose
+    return(
+        <div className="p-1 shadow-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-2xl">
+          <div className=" bg-black sm:p-10 p-6 rounded-xl">
+            <div className="">
+              <h5 className="text-xl font-bold text-gray-200">{topic || "Science of Chemistry"}</h5>
+    
+              <p className="mt-2 text-sm text-gray-400">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, adipisci.
+              </p>
+            </div>
+          </div>
+        </div>
+
+    )
+}
+export default Card;
